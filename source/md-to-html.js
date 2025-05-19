ea0831e1e850e004e9da9b6527df5c5027d39bf3
Andromeda <3x1t_5tyl3@protonmail.ch>
2025-05-19 11:07:52 +0200

    updated js
    should generate decently. It's by no means perfect tho.

--- source/md-to-html.js
+++ source/md-to-html.js
@@ -1,8 +1,34 @@
+// this entire file is a clsuterfuck and i dont care. i used a bit of mistral.ai to help me with regex and the manual toc shit cause fuck that lmao.
 const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
 
 const mdFolderPath = path.join(__dirname, 'md');
+
+function processMarkdown(markdownText) {
+    // honestly fuck this regex, but it works lol
+    const headingRegex = /^(#+)\s+(.*?)\s*$/gm;
+    let match;
+    let toc = '## Table of Contents\n\n';
+    let lastIndex = 0;
+    let modifiedMarkdown = '';
+
+    // heading didn't work properly. idk. it's good enough ¯\_(ツ)_/¯
+    while ((match = headingRegex.exec(markdownText)) !== null) {
+        modifiedMarkdown += markdownText.substring(lastIndex, match.index);
+
+        const level = match[1].length;
+        const title = match[2].trim();
+        const anchor = title.toLowerCase().replace(/\s+/g, '-');
+
+        // AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+        modifiedMarkdown += `${match[1]} <a name="${anchor}"></a>${title}\n`;
+        toc += `${'  '.repeat(level - 1)}- [${title}](#${anchor})\n`;
+        lastIndex = headingRegex.lastIndex;
+    }
+    modifiedMarkdown += markdownText.substring(lastIndex);
+    return `${toc}\n\n${modifiedMarkdown}`;
+}
 
 fs.readdir(mdFolderPath, (err, files) => {
     if (err) {
@@ -13,15 +39,13 @@ fs.readdir(mdFolderPath, (err, files) => {
     files.forEach(file => {
         if (path.extname(file) === '.md') {
             const mdFilePath = path.join(mdFolderPath, file);
-
             fs.readFile(mdFilePath, 'utf8', (err, markdownText) => {
                 if (err) {
                     console.error(`Error reading file ${file}:`, err);
                     return;
                 }
-
-                const htmlContent = marked.parse(markdownText);
-
+                const processedMarkdown = processMarkdown(markdownText);
+                const htmlContent = marked.parse(processedMarkdown);
                 const htmlFileName = file.replace('.md', '.html');
                 const htmlFilePath = path.join(__dirname, htmlFileName);
 
